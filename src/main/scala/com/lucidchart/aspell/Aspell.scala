9776d5dc159163bc7b2e273b0fa1731bd2b37818
Mark Siebert <mark@lucidchart.com>
2015-05-04 09:14:44 -0600

    add some comments and style changes

--- src/main/scala/com/lucidchart/aspell/Aspell.scala
+++ src/main/scala/com/lucidchart/aspell/Aspell.scala
@@ -1,6 +1,8 @@
 package com.lucidchart.aspell
 
-class Aspell { 
+import scala.util.Try
+
+private[aspell] class Aspell {
 
   // initialize aspell for a given language
   @native def init(language: String): Boolean
@@ -14,27 +16,56 @@ class Aspell {
   // Add user words to dictionary
   @native def addUserWords(words: Array[String])
 
-  // This is going to be cast to an aspell pointer in the native(C and C++) code. 
+  // This is going to be cast to an aspell pointer in the native(C and C++) code.
   var aspellPtr: Long = 0;
 }
 
+/**
+ * Case class representation of the result sent back from the aspell library.
+ *
+ * @param word the word that was checked
+ * @param valid whether or not the word was correctly spelled
+ * @param suggestions if the word is not valid, this array will contain suggested spellings
+ */
 case class WordSuggestions(word: String, valid: Boolean, suggestions: Array[String])
 
+/**
+ * The Aspell object provides the public interface for the lucid-aspell library. It loads the native
+ * library and has a method to check the spelling on a word.
+ */
 object Aspell {
   NativeLibraryLoader.load(s"/${BuildInfo.libraryName}.so")
+
+  /**
+   * Check the spelling for each word in an array of words
+   *
+   * @param language the language code for the dictionary to use (ie, en, es, fr, etc)
+   * @param words the words to check
+   * @param userWords the custom words that the user has created to augment the dictionary of
+   * correct words
+   * @return an array of WordSuggestions object, with one WordSuggestions for each word that was
+   * checked
+   */
   def check(language: String, words: Array[String], userWords: Array[String]): Array[WordSuggestions] = {
     val aspell = new Aspell
     aspell.init(language)
-    if (!userWords.isEmpty) 
+
+    if (!userWords.isEmpty) {
       aspell.addUserWords(userWords)
-    val checks = words.map{ word =>
-      val wordCheck = aspell.check(word)
-      val valid = if (wordCheck(0).toInt == 1) true else false
-      if (wordCheck.length >= 1)
-        WordSuggestions(word, valid, wordCheck.slice(1, wordCheck.length))
-      else
-        WordSuggestions(word, false, Array())
     }
+    val checks = words.map { word =>
+      val wordCheck = aspell.check(word)
+      val valid = Try {
+        if (wordCheck(0).toInt == 1) true else false
+      }.getOrElse(false)
+      if (wordCheck.length >= 1) {
+        WordSuggestions(word, valid, wordCheck.slice(1, wordCheck.length))
+      }
+      else {
+        WordSuggestions(word, false, Array.empty)
+      }
+    }
+
     aspell.cleanup
     checks
   }
